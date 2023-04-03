a7bb06d203177bb86ce076b8f70aa4e9d099d396
Wilfor Ibanez <aln.wil4@outlook.es>
2023-04-03 17:53:37 -0400

    Working on Creation Form with bootstrap classes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: ProductsListComponent},
+  {path:'create-product', component: CreateProductComponent},
+  {path:'edit-product/:id', component: CreateProductComponent},
+  {path:'**', redirectTo:'', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Components
+import { CreateProductComponent } from './components/create-product/create-product.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateProductComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule,
